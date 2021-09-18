e2dca96127aad2b4021a9430da55837e614dc43c
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-09-18 15:13:06 +0900

    update

--- app/src/main/java/com/example/databindingtips/MainActivity.kt
+++ app/src/main/java/com/example/databindingtips/MainActivity.kt
@@ -9,5 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        // 現在時刻をセットする
+        
     }
 }
