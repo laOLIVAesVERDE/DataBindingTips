3fe12984cd6f2a2597b586106e9d0aa0489fffe8
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-09-22 00:01:57 +0900

    update

--- app/src/main/java/com/example/databindingtips/MyCustomView.kt
+++ app/src/main/java/com/example/databindingtips/MyCustomView.kt
@@ -5,7 +5,11 @@ import android.util.AttributeSet
 import android.widget.FrameLayout
 
 class MyCustomView : FrameLayout {
-    constructor(context: Context) : super(context, null)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context) : super(context, null) { initView() }
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) { initView() }
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) { initView() }
+
+    private fun initView() {
+        inflate(context, R.layout.my_custom_view, this);
+    }
 }
--- app/src/main/java/com/example/databindingtips/User.kt
+++ app/src/main/java/com/example/databindingtips/User.kt
@@ -1,5 +1,7 @@
 package com.example.databindingtips
 
 class User(val name : String, val year : Int) {
-
+    private fun introduce() : String {
+        return "I'm $name, $year years old."
+    }
 }
