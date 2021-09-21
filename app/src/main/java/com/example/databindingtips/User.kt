d6944a6c27a271af8d44aec3da2538a50f2d40b5
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-09-22 00:41:31 +0900

    Update User.kt

--- app/src/main/java/com/example/databindingtips/User.kt
+++ app/src/main/java/com/example/databindingtips/User.kt
@@ -1,7 +1,5 @@
 package com.example.databindingtips
 
 class User(val name : String, private val year : Int) {
-    fun introduce() : String {
-        return "I'm $name, $year years old."
-    }
+    fun introduce() : String = "I'm $name, $year years old."
 }
