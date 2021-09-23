bc3298066efaa7d6b67a33393f39c35b4bee80e0
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-09-23 21:59:03 +0900

    update

--- app/src/main/java/com/example/databindingtips/AppUtil.kt
+++ app/src/main/java/com/example/databindingtips/AppUtil.kt
@@ -7,8 +7,8 @@ object AppUtil {
     @JvmStatic
     fun convertToLocalTimeFormat(): String {
         val format = when (Locale.getDefault().language) {
-            "ja" -> SimpleDateFormat("yyyy年M月d日 HH時mm分", Locale.getDefault())
-            else -> SimpleDateFormat("M/d/yyyy HH:mm", Locale.getDefault())
+            "ja" -> SimpleDateFormat("yyyy年M月d日 H時mm分", Locale.getDefault())
+            else -> SimpleDateFormat("M/d/yyyy H:mm", Locale.getDefault())
         }
         val date = Date(System.currentTimeMillis())
         return format.format(date)
